f2025e30bd507f2f3dcd31e020389b3e81e77f8d
Denys Liubarets <denys.liubarets@outlook.com>
2022-02-15 01:01:02 +0200

    Initial commit

---
+++ SimpleSelect.Tests/Helpers/SelectHelper.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Data.Sqlite;
+using SqlDataInsert.Tests.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SqlDataInsert.Tests.Helpers
+{
+    internal class SelectHelper
+    {
+        public static SelectResult[] GetResults(IEnumerable<string> queries)
+        {
+            var results = new List<SelectResult>();
+            foreach (var query in queries)
+            {
+                var result = GetResult(query);
+                results.Add(result);
+            }
+            return results.ToArray();
+        }
+
+        public static SelectResult GetResult(string query)
+        {
+            var command = new SqliteCommand(query, SqliteHelper.Connection);
+            var result = new SelectResult();
+            try
+            {
+                var reader = command.ExecuteReader();
+                result = Read(reader);
+            }
+            catch (Exception e)
+            {
+                result.ErrorMessage = e.Message;
+            }
+            return result;
+        }
+
+        public static void SerializeResult(SelectResult selectResult, string file)
+        {
+            File.WriteAllText(file, selectResult.ToString());
+        }
+
+        public static SelectResult DeserializeResult(string file)
+        {
+            if (!File.Exists(file))
+                throw new FileNotFoundException($"The file '{file}' was not found in the 'Data' folder.");
+            var lines = File.ReadAllLines(file);
+            var schema = lines[0].Split(",");
+            var types = lines[1].Split(",");
+            var data = new List<string[]>();
+            for (var i = 2; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                data.Add(line.Split(","));
+            }
+            return new SelectResult
+            {
+                Schema = schema,
+                Types = types,
+                Data = data.ToArray()
+            };
+        }
+
+        private static SelectResult Read(SqliteDataReader reader)
+        {
+            var data = new List<string[]>();
+            var result = new SelectResult(reader.FieldCount);
+            while (reader.Read())
+            {
+                var rowData = new string[reader.FieldCount];
+                for (var i = 0; i < reader.FieldCount; i++)
+                {
+                    if (data.Count == 0)
+                    {
+                        result.Schema[i] = reader.GetName(i);
+                        result.Types[i] = reader.GetDataTypeName(i);
+                    }
+                    rowData[i] = reader.GetString(i);
+                }
+                data.Add(rowData);
+            }
+            result.Data = data.ToArray();
+            return result;
+        }
+    }
+}
---
+++ SimpleSelect.Tests/SqlTaskTests.cs
@@ -0,0 +1,132 @@
+﻿using NUnit.Framework;
+using SqlDataInsert.Tests.Helpers;
+using System;
+using SqlDataInsert.Tests.Models;
+using System.IO;
+
+namespace SqlDataInsert.Tests
+{
+    [TestFixture]
+    public class SqlTaskTests
+    {
+        private const int FilesCount = 5;
+        private static readonly string ProjectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+        private static readonly string DatabaseFile = Path.Combine(ProjectDirectory, "DB", "marketplace.db");
+        private static readonly string[] FileNames = { "task1.sql", "task2.sql", "task3.sql", "task4.sql", "task5.sql" };
+        private static readonly string[] QueryFiles = SqlTask.GetFilePaths(FileNames);
+        private static readonly string[] Queries = QueryHelper.GetQueries(QueryFiles);
+        private static SelectResult[] ActualResults;
+        private static SelectResult[] ExpectedResults;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            SqliteHelper.OpenConnection(DatabaseFile);
+            DeserializeResultFiles();
+            ActualResults = SelectHelper.GetResults(Queries);
+        }
+
+        [OneTimeTearDown]
+        public void Cleanup()
+        {
+            SqliteHelper.CloseConnection();
+        }
+
+        [Test]
+        public void FileWithQueries_Exists([Range(0, FilesCount - 1)] int index)
+        {
+            AssertFileExist(index);
+        }
+
+        [Test]
+        public void FileWithQueries_NotEmpty([Range(0, FilesCount - 1)] int index)
+        {
+            AssertFileNotEmpty(index);
+        }
+
+        [Test]
+        public void AllInsertQueries_ExecuteSuccessfully([Range(0, FilesCount - 1)] int index)
+        {
+            AssertData(index);
+        }
+
+        [Test]
+        public void SelectQuery_ReturnsCorrectRowsCount([Range(0, FilesCount - 1)] int index)
+        {
+            AssertData(index);
+            var expected = ExpectedResults[index].Data.Length;
+            var actual = ActualResults[index].Data.Length;
+            Assert.AreEqual(expected, actual, ActualResults[index].ErrorMessage);
+        }
+
+        [Test]
+        public void SelectQuery_ReturnsCorrectSchema([Range(0, FilesCount - 1)] int index)
+        {
+            AssertData(index);
+            var expected = ExpectedResults[index].Schema;
+            var actual = ActualResults[index].Schema;
+            var expectedMessage = MessageComposer.Compose(expected);
+            var actualMessage = MessageComposer.Compose(actual);
+            Assert.AreEqual(expected, actual, "\nExpected:\n{0}\n\nActual:\n{1}\n", expectedMessage, actualMessage);
+        }
+
+        [Test]
+        public void SelectQuery_ReturnsCorrectTypes([Range(0, FilesCount - 1)] int index)
+        {
+            AssertData(index);
+            var expected = ExpectedResults[index].Types;
+            var actual = ActualResults[index].Types;
+            var expectedMessage = MessageComposer.Compose(expected);
+            var actualMessage = MessageComposer.Compose(actual);
+            Assert.AreEqual(expected, actual, "\nExpected:\n{0}\n\nActual:\n{1}\n", expectedMessage, actualMessage);
+        }
+
+        [Test]
+        public void SelectQuery_ReturnsCorrectData([Range(0, FilesCount - 1)] int index)
+        {
+            AssertData(index);
+            var expected = ExpectedResults[index].Data;
+            var actual = ActualResults[index].Data;
+            var expectedMessage = MessageComposer.Compose(ExpectedResults[index].Schema, expected);
+            var actualMessage = MessageComposer.Compose(ActualResults[index].Schema, actual);
+            Assert.AreEqual(expected, actual, "\nExpected:\n{0}\n\nActual:\n{1}\n", expectedMessage, actualMessage);
+        }
+
+        private static void AssertData(int index)
+        {
+            AssertFileExist(index);
+            AssertFileNotEmpty(index);
+            AssertErrors(index);
+        }
+
+        private static void AssertErrors(int index)
+        {
+            if (!string.IsNullOrEmpty(ActualResults[index].ErrorMessage))
+                Assert.Fail(ActualResults[index].ErrorMessage);
+        }
+
+        private static void AssertFileExist(int index)
+        {
+            var actual = Queries[index];
+            var message = $"The file '{FileNames[index]}' was not found.";
+            if (actual == null)
+                Assert.Fail(message);
+        }
+
+        private static void AssertFileNotEmpty(int index)
+        {
+            var actual = Queries[index];
+            var message = $"The file '{FileNames[index]}' contains no entries.";
+            if (string.IsNullOrWhiteSpace(actual))
+                Assert.Fail(message);
+        }
+
+        private static void DeserializeResultFiles()
+        {
+            var files = Directory.GetFiles(Path.Combine(ProjectDirectory, "Data"), "*.csv");
+            ExpectedResults = new SelectResult[FilesCount];
+            for (var i = 0; i < FilesCount; i++)
+                ExpectedResults[i] = SelectHelper.DeserializeResult(files[i]);
+        }
+    }
+}

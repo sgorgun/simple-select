42316b4e441f0d8aa07798f029c1ff97e8a8e6b1
Yuriy Antonov <y.s.antonov.edu@gmail.com>
2022-03-20 19:42:46 +0200

    Agregete helpers, mpdels and parsers. Change DB schema.

--- SimpleSelect.Tests/Helpers/SqliteHelper.cs
+++ SimpleSelect.Tests/Helpers/SqliteHelper.cs
@@ -1,5 +1,7 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
 using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
 
 namespace AutocodeDB.Helpers
 {
@@ -13,6 +15,18 @@ namespace AutocodeDB.Helpers
             Connection = new SqliteConnection(connectionString);
         }
 
+        public static void OpenConnection()
+        {
+            var connectionString = new SqliteConnectionStringBuilder
+            {
+                Mode = SqliteOpenMode.Memory,
+                ForeignKeys = true,
+
+            }.ConnectionString;
+
+            Connection = new SqliteConnection(connectionString);
+            Connection.Open();
+        }
         public static void OpenConnection(string file)
         {
             Connection.Open();
@@ -52,5 +66,25 @@ namespace AutocodeDB.Helpers
             DataSource = fileName,
             Mode = readOnly ? SqliteOpenMode.ReadOnly : SqliteOpenMode.ReadWriteCreate
         }.ConnectionString;
+
+        public static int CountRows(string tableName)
+        {
+            var countCmd = new SqliteCommand($"SELECT COUNT(*) FROM {tableName}", Connection);
+            return Convert.ToInt32(countCmd.ExecuteScalar());
+        }
+
+        public static int[] CountRows(string tableName, string columnName)
+        {
+            var countCmd = new SqliteCommand($"SELECT {columnName}, COUNT({columnName}) FROM {tableName} GROUP BY {columnName}", Connection);
+            var reader = countCmd.ExecuteReader();
+            List<int> ListRes = new List<int>();
+            while (reader.Read())
+            {
+                ListRes.Add(reader.GetInt32(1));
+            }
+            return ListRes.ToArray();
+        }
+
     }
+
 }
